a40eb61c83be02bd2b2f2c73ccc44929043c59cb
basselldeeb <basselldeeb@gmail.com>
2015-11-22 11:01:50 -0500

    Week 2 Assignment for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The goal of this code is to compute the inverse of a matrix if it has not already been calculated
+##If it has already been calculated, then the code will refer to the cache to identify the values
 
-## Write a short comment describing this function
+##The makeCacheMatrix function will create a special "matrix" objects that can cahce its inverse
+makeCacheMatrix <- function(x = matrix()) 
+  {
+  invMat <- NULL
+  set <- function(y) {
+    x <<- y
+    invMat <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invMat <<- inverse
+  getinverse <- function() invMat
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+## The cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above.  
+## if the inverse has already been computed, it will be retrieved from the cache. 
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) 
+  {
+  invMat <- x$getinverse()
+  if(!is.null(invMat)) 
+    {
+    message("getting cached data!")
+    ## Return a matrix that is the inverse of 'x'
+    return(invMat)
+    }
+  data <- x$get()
+  invMat <- solve(data)
+  x$setinverse(invMat)
+  invMat
+        
+  }
